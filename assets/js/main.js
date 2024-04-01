c8d197df857a478774c07f4e3cc506730de05497
Carlos Febres <febres0492@gmail.com>
2024-04-01 19:48:06 -0400

    adding logic to append tasks cards

--- assets/js/main.js
+++ assets/js/main.js
@@ -11,11 +11,26 @@ $('#task-form').submit((ev)=> {
     $('#task-modal').modal('hide');
 
     //get task data
-    let tasTitle = $('#task-title').val();
-    let taskDescription = $('#task-description').val();
+    let tasTitle = capFirst($('#task-title').val());
+    let taskDescription = capFirst($('#task-description').val());
     let taskDate = $('#task-due-date').val();
 
-    
-    console.log('test',tasTitle, taskDescription, taskDate);
+    const str = `
+        <div class="card bg-light mb-3 cur-move" style="max-width: 18rem;">
+            <div class="card-header p-1 fw-bold">${tasTitle}</div>
+            <div class="card-body p-2">
+                <p class="card-text">${taskDescription}</p>
+                <p class="card-date">${taskDate}</p>
+                <button class="delete-btn btn btn-secondary p-1 px-3">Delete</button>
+            </div>
+        </div>
+    `
+    $('#task-todo-col').append(str);
 })
+
+
+// capFirstLetter
+function capFirst(str) {
+    return str.charAt(0).toUpperCase() + str.slice(1);
+}
 
