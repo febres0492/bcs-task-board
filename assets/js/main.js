fbc2092bec7a5705d312b15f04b99bd78e58d941
Carlos Febres <febres0492@gmail.com>
2024-04-02 19:52:27 -0400

    adding logic to change card colors

--- assets/js/main.js
+++ assets/js/main.js
@@ -11,13 +11,33 @@ $('#task-form').submit((ev)=> {
     // hidding modal
     $('#task-modal').modal('hide');
 
-    //get task data
+    //geting task data
     let tasTitle = capFirst($('#task-title').val());
     let taskDescription = capFirst($('#task-description').val());
-    let taskDate = $('#task-due-date').val();
+    let taskDate = $('#task-due-date').val()
+
+    // formating task date to compare to current date
+    let formattedTaskDate = parseInt(taskDate.split('-').map(v => parseInt(v)).join(''))
+
+    // getting current date
+    let t = new Date();
+    const currentDate = parseInt([t.getFullYear(), t.getMonth()+1, t.getDate()].join(''))
+
+    //default color
+    let color = 'light';
+
+    // Checking if the task date is in the past
+    if (formattedTaskDate < currentDate) {
+        color = 'danger';
+    } 
+
+    // Checking if the task date is in the future
+    if (formattedTaskDate == currentDate) {
+        color = 'warning';
+    }
 
     const str = `
-        <div class="card bg-light mb-3 cur-move" style="max-width: 18rem;">
+        <div class="card bg-${color} mb-3 cur-move" style="max-width: 18rem;">
             <div class="card-header p-1 fw-bold">${tasTitle}</div>
             <div class="card-body p-2">
                 <p class="card-text">${taskDescription}</p>
@@ -30,19 +50,25 @@ $('#task-form').submit((ev)=> {
 })
 
 // showing draggable card
-$('#task-todo-col').mousemove((ev) => {
+$('#task-todo-col').mousedown((ev) => {
     if(ev.buttons !== 1) return; // return ifleft mouse button is not pressed
     
     const card = ev.target.closest('.card');
     $(card).draggable({ 
         helper: ()=>{
             // making clode the same width as the card
-            return $(card).clone().width($(card).outerWidth());
+            const cloneCard = $(card).clone().width($(card).outerWidth());
+            cloneCard.css('z-index', '20');
+            return cloneCard;
+        },
+        start: () => {
+            // changing card opacity
+            $(card).css('opacity', '0.5')
         },
         stop: (ev2) => {
             const pointerPos = { x: ev2.pageX, y: ev2.pageY };
 
-            //getting target card
+            //getting target car d
             const targetColumn = $('.task-column').get().reverse().find(el => {
                 const elPos = $(el).offset();
                 if(elPos.top < pointerPos.y && elPos.left < pointerPos.x) {
@@ -53,12 +79,11 @@ $('#task-todo-col').mousemove((ev) => {
             //appending card to target card
             if(targetColumn){
                 $(targetColumn).append(card);
+                $(card).css('opacity', '1')
             }
         }
     });
 });
-
-
 
 // capFirstLetter
 function capFirst(str) {
